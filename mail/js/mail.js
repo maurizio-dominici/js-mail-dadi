1db62cf0b140dd7c5bf9630ab9745a1400fd2ed1
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-04 15:52:33 +0200

    ex mail

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,1 +1,26 @@
+const suffix = "@gmail.com";
+const emailList = [
+  `maurizio ${suffix}`,
+  `federico ${suffix}`,
+  `massimo ${suffix}`,
+  `andrea ${suffix}`,
+  `luca ${suffix}`,
+  `antonio ${suffix}`,
+  `alessia ${suffix}`,
+  `francesca ${suffix}`,
+];
 
+const emailUser = prompt("inserisci la tua mail");
+
+// console.table(emailList, typeof emailList);
+// console.log(emailUser);
+
+for (let i = 0; i < emailList.length; i++) {
+  //   console.log("ciao");
+
+  if (emailUser === emailList[i]) {
+    console.log("accesso effettuato");
+  }
+  //   const currentEmail = emailList[i];
+  //   console.table(currentEmail);
+}
