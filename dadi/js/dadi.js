80f6e0e8e53cca71417a49ebd18fd15a890a8174
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-04 17:19:25 +0200

    ex dadi

--- dadi/js/dadi.js
+++ dadi/js/dadi.js
@@ -1,1 +1,18 @@
-console.log("ciao");
+// GENERO DEI NUMMERI RANDOM PER L'UTENTE E PER IL COMPUTER
+
+// console.log(randomUser);
+
+const randomPc = Math.floor(Math.random() * 7);
+
+console.log("randomPc", randomPc);
+
+const randomUser = Math.floor(Math.random() * 7);
+console.log("randomUser", randomUser);
+
+let outputText = "user vince";
+
+if (randomPc >= randomUser) {
+  outputText = "Pc vince";
+}
+
+console.log(outputText);
